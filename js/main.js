2dd74eae268a5966d87e60872ca1e50aa5b6d4f5
Ali Beji <41632688+alibeji@users.noreply.github.com>
2018-11-27 22:28:54 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function exDes(){
+	document.getElementById('gdesign').classList.toggle("show");
+	}
+function exIt(){
+	document.getElementById('it').classList.toggle("show");
+	}
+function exLg(){
+	document.getElementById('lg').classList.toggle("show");
+}
+function aiDet(){
+	document.getElementById('aiesecdetails').classList.toggle("show");
+}
